9431360ef135cc40a6b75bf16c612210cd3102a4
shamil <moidu4886@gmail.com>
2026-05-19 01:46:42 +0530

    Update admin.py

--- bot/filters/admin.py
+++ bot/filters/admin.py
@@ -1,14 +1,26 @@
-from typing import Union
-
 from aiogram.filters import BaseFilter
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import Message, CallbackQuery
 
 from bot.config import config
 
 
 class IsAdmin(BaseFilter):
-    async def __call__(self, event: Union[Message, CallbackQuery]) -> bool:
-        user = event.from_user
+    async def __call__(self, event) -> bool:
+        user = None
+
+        if isinstance(event, Message):
+            user = event.from_user
+
+        elif isinstance(event, CallbackQuery):
+            user = event.from_user
+
+        elif getattr(event, "message", None):
+            user = event.message.from_user
+
+        elif getattr(event, "callback_query", None):
+            user = event.callback_query.from_user
+
         if not user:
             return False
+
         return user.id in config.admin_ids
